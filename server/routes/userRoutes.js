6b988da063bd0b089988b219a1da1986ec0693d9
Tommy Sutjipto <tommysutjipto96@gmail.com>
2019-07-18 17:38:08 +0700

    multer

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router()
 const UserController = require('../controllers/userController')
+const images = require('../helpers/images')
 
 router.post('/register',UserController.login)
 router.post('/login',UserController.create)
 router.post('/googleSignIn',UserController.googleSignIn)
+
+router.post('/googleCloudStorage', 
+    images.multer.single('file'), 
+    images.sendUploadToGCS,
+    (req,res,next)=>{
+      console.log(req.file)
+        let imageLink = req.file.cloudStoragePublicUrl
+        console.log(imageLink)
+        res.status(200).json(imageLink)
+    }
+)
 
 module.exports = router
